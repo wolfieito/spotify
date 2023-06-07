1c46d05b05f7c9674dc6b32b71afdd9d37e7ae58
wolfieito <54371752+wolfieito@users.noreply.github.com>
2023-06-07 15:25:31 -0600

    Listas de link izquierda

---
+++ action.js
@@ -0,0 +1,11 @@
+var linkContainer = document.getElementById("linkList");
+
+var links = linkContainer.getElementsByClassName("link");
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
